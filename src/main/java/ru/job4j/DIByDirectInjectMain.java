60a0c3dffdad4beb066e356d64a663de7f926e9e
alex <differentplayer@yandex.ru>
2025-05-27 02:56:50 +0700

    Внедрение коллекций | Поставщики контекста [#505300 #544694]

--- src/main/java/ru/job4j/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/DIByDirectInjectMain.java
@@ -8,6 +8,6 @@ public class DIByDirectInjectMain {
 	public static void main(String[] args) {
 		var handler = new BotCommandHandler();
 		var tg = new TelegramBotService(handler);
-		tg.receive(new Content());
+		tg.receive(new Content(1L));
 	}
 }
