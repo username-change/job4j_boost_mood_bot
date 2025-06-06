7ec94a15fe24e6e53bc369a4028916d28b30456a
alex <differentplayer@yandex.ru>
2025-06-07 02:44:10 +0700

    SentContent - интеграция Content с Telegram API [#505307]

--- src/main/java/ru/job4j/bot/TelegramBotService.java
+++ src/main/java/ru/job4j/bot/TelegramBotService.java
@@ -1,29 +1,74 @@
 package ru.job4j.bot;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendAudio;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.Update;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import ru.job4j.aspect.SentContentException;
 import ru.job4j.content.Content;
+import ru.job4j.service.SentContent;
 
-@Component
-public class TelegramBotService {
+@Service
+public class TelegramBotService extends TelegramLongPollingBot implements SentContent {
 	private final BotCommandHandler handler;
+	private final String botName;
 
-	public TelegramBotService(BotCommandHandler handler) {
+	public TelegramBotService(@Value("${telegram.bot.name}") String botName,
+								@Value("${telegram.bot.token}") String botToken,
+								BotCommandHandler handler) {
+		super(botToken);
 		this.handler = handler;
+		this.botName = botName;
 	}
 
-	public void receive(Content content) {
+	@Override
+	public void onUpdateReceived(Update update) {
+		if (update.hasCallbackQuery()) {
+			handler.handleCallback(update.getCallbackQuery()).ifPresent(this::sent);
+		} else if (update.hasMessage() && update.getMessage().getText() != null) {
+			handler.commands(update.getMessage()).ifPresent(this::sent);
+		}
 	}
 
-	@PostConstruct
-	public void init() {
-		System.out.println("Bean is going through init.");
+	@Override
+	public String getBotUsername() {
+		return botName;
 	}
 
-	@PreDestroy
-	public void destroy() {
-		System.out.println("Bean will be destroyed now.");
+	@Override
+	public void sent(Content content) {
+	    try {
+	        if (content.getAudio() != null) {
+	            SendAudio sendAudio = new SendAudio();
+	            sendAudio.setChatId(content.getChatId());
+	            sendAudio.setAudio(content.getAudio());
+	            if (content.getText() != null) {
+	                sendAudio.setCaption(content.getText());
+	            }
+	            execute(sendAudio);
+	        } else if (content.getPhoto() != null) {
+	            SendPhoto sendPhoto = new SendPhoto();
+	            sendPhoto.setChatId(content.getChatId());
+	            sendPhoto.setPhoto(content.getPhoto());
+	            if (content.getText() != null) {
+	                sendPhoto.setCaption(content.getText());
+	            }
+	            execute(sendPhoto);
+	        } else if (content.getText() != null && !content.getText().isEmpty()) {
+	            SendMessage sendMessage = new SendMessage();
+	            sendMessage.setChatId(content.getChatId());
+	            sendMessage.setText(content.getText());
+	            if (content.getMarkup() != null) {
+	                sendMessage.setReplyMarkup(content.getMarkup());
+	            }
+	            execute(sendMessage);
+	        }
+	    } catch (Exception e) {
+			throw new SentContentException("Ошибка при отправке контента", e);
+		}
 	}
 }
---
+++ src/main/java/ru/job4j/service/SentContent.java
@@ -0,0 +1,7 @@
+package ru.job4j.service;
+
+import ru.job4j.content.Content;
+
+public interface SentContent {
+	void sent(Content content);
+}
